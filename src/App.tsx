35f8941b7c93e0e7e12777d088892cde048c6d41
unknown <alaodev@gmail.com>
2021-10-24 18:33:34 -0300

    first commit]

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { MessageList } from './components/MessageList';
+import { LoginBox } from './components/LoginBox';
+import { useContext } from 'react';
+import { AuthContext } from './contexts/auth';
+
+import styles from './App.module.scss';
+import { SendMessageForm } from './components/SendMessageForm';
+
+export function App() {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <main className={`${styles.contentWrapper} ${!!user ? styles.contentSigned : ''}`}>
+      <MessageList />
+      {!!user ? <SendMessageForm /> : <LoginBox />}
+    </main>
+  )
+}
---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,21 @@
+import { useContext, useEffect } from 'react';
+import { VscGithubInverted } from 'react-icons/vsc';
+import { AuthContext } from '../../contexts/auth';
+
+import { api } from '../../services/api';
+
+import styles from './styles.module.scss';
+
+export function LoginBox() {
+  const { signInUrl } = useContext(AuthContext);
+
+  return (
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href={signInUrl} className={styles.signWithGithub}>
+        <VscGithubInverted size="24" />
+        Entrar com Github
+      </a>
+    </div>
+  )
+}
